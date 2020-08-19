3249099c5b31f92c3dc56be2bdbabf6abb8b2de5
Kevin Gann <kevin.gann@rev.com>
2020-08-19 09:50:29 -0400

    Removing build plugins from pom.xml. Bumping version of junit.

---
+++ src/test/java/com/kaygee/FirstTest.java
@@ -0,0 +1,13 @@
+package com.kaygee;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FirstTest {
+
+  @Test
+  public void helloWorld() {
+    assertTrue(true);
+  }
+}
